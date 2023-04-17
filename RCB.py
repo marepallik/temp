aa961ffaa44b89a05bdc8cec9256fe955fe485f6
Keerthana marepalli <106551551+marepallik@users.noreply.github.com>
2023-04-17 22:35:49 +0530

    Create RCB.py
    ipl hawaa

---
+++ RCB.py
@@ -0,0 +1,7 @@
+ipl_team=input('enter ipl team :')
+match ipl_team:
+    case "RCB":
+        print("eesala cup nahi hai")
+        print("next time lol")
+    case default:
+        print('we won the match')
